78d7c4932b4c2898cdc84797d0989fbe215ed5ce
v4sh6n <v4sh6n@gmail.com>
2023-05-11 12:21:53 +0200

    test new

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,4 +1,20 @@
 const http = require('http');
+const { spawn } = require('child_process');
+
+let server;
+
+beforeAll((done) => {
+  server = spawn('ng', ['serve', '--port', '8080']);
+  server.stdout.on('data', (data) => {
+    if (data.includes('Development Server is listening on')) {
+      done();
+    }
+  });
+});
+
+afterAll(() => {
+  server.kill();
+});
 
 test('La respuesta de la petición HTTP es un 200 y el texto recibido es "Hola mundo"', (done) => {
   http.get('http://localhost:8080', (res) => {
@@ -13,3 +29,20 @@ test('La respuesta de la petición HTTP es un 200 y el texto recibido es "Hola m
     });
   });
 });
+
+
+// const http = require('http');
+
+// test('La respuesta de la petición HTTP es un 200 y el texto recibido es "Hola mundo"', (done) => {
+//   http.get('http://localhost:8080', (res) => {
+//     expect(res.statusCode).toBe(200);
+//     let data = '';
+//     res.on('data', (chunk) => {
+//       data += chunk;
+//     });
+//     res.on('end', () => {
+//       expect(data).toBe('Hola mundo');
+//       done();
+//     });
+//   });
+// });
